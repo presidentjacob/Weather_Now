8fa54b4fdcefa1458fa4eb17606fe457966f3e47
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-21 23:09:55 -0600

    Create members page

--- weather_website/members/models.py
+++ weather_website/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    city = models.CharField(max_length=30, default='Unknown')
+    state = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
