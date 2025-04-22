9ed21a25822229b66d17b8fd40e13c791c833a1a
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-21 23:59:31 -0600

    Work on the user_form and weather_info webpages

--- weather_website/members/urls.py
+++ weather_website/members/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('user_form/', views.user_form, name='user_form'),
+    path('weather_info/', views.weather_info, name='weather_info'),
 ]
--- weather_website/members/views.py
+++ weather_website/members/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+import requests
+from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver
+import time
+import lxml
 
 # Create your views here.
 def members(request):
@@ -19,3 +25,54 @@ def details(request, id):
         'mymember': mymember,
     }
     return HttpResponse(template.render(context, request))
+
+def user_form(request):
+    if request.method == 'POST':
+        return render(request, 'user_form.html')
+    else:
+        return render(request, 'user_form.html')
+    
+def weather_info(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        city = request.POST['city']
+        state = request.POST['state']
+        country = request.POST['country']
+        
+        # Construct the URL for the weather API
+        url = f"https://www.google.com/search?q={city}+{state}+{country}+weather"
+        
+        # Use Selenium and BS4 to fetch the weather information
+        driver_options = webdriver.ChromeOptions()
+        driver_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=driver_options)
+
+        driver.get(url)
+        # Wait for page to load
+        time.sleep(5)
+
+        html = driver.page_source
+
+        soup = BeautifulSoup(html, 'lxml')
+        driver.quit()
+        temperature_f = soup.find('span', {'id': 'wob_tm'}).text
+        temperature_c = soup.find('span', {'id': 'wob_ttm'}).text
+        precipitation = soup.find('span', {'id': 'wob_pp'}).text
+        humidity = soup.find('span', {'id': 'wob_hm'}).text
+        wind = soup.find('span', {'id': 'wob_ws'}).text
+
+        context = {
+            'firstname': firstname,
+            'lastname': lastname,
+            'city': city,
+            'state': state,
+            'country': country,
+            'temperature_f': temperature_f,
+            'temperature_c': temperature_c,
+            'precipitation': precipitation,
+            'humidity': humidity,
+            'wind': wind
+        }
+
+    return render(request, 'weather_info.html')
